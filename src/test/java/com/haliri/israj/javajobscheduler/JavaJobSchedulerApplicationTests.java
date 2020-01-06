ef765b254bac9c948e4635f4d92e9f430a00a844
Israj Haliri (ID) <israj.haliri@HCIDs-MacBook-Pro-9.local>
2020-01-06 10:32:57 +0700

    first commit

---
+++ src/test/java/com/haliri/israj/javajobscheduler/JavaJobSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haliri.israj.javajobscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJobSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
